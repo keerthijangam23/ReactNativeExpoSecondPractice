d15309894f062c6a654105f5803bdbe4cd147910
keerthi <jangamkeerthi26@gmail.com>
2024-04-18 16:27:47 +0530

    Add pull to refresh feature

--- Components/GetRequestApi.tsx
+++ Components/GetRequestApi.tsx
@@ -1,18 +1,42 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 import { useEffect, useState } from "react";
 
 export default function GetRequestApi() {
   const [postList, setPostList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
   const fetchData = async (limit = 10) => {
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
     );
     const data = await response.json();
     setPostList(data);
+    setIsLoading(false);
+  };
+  const handleRefresh = () => {
+    setRefreshing(true);
+    fetchData(20);
+    setRefreshing(false);
   };
   useEffect(() => {
     fetchData();
   }, []);
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="0000ff" />
+        <Text>Loading ...</Text>
+      </SafeAreaView>
+    );
+  }
   return (
     <View style={styles.listContainer}>
       <FlatList
@@ -31,6 +55,8 @@ export default function GetRequestApi() {
         ListEmptyComponent={<Text>No Posts Found</Text>}
         ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
         ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
+        refreshing={refreshing}
+        onRefresh={handleRefresh}
       />
     </View>
   );
@@ -53,14 +79,21 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: "brown",
   },
-  headerText:{
-    fontSize:24,
-    textAlign:"center",
-    marginBottom:12
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
   },
-  footerText:{
-    fontSize:24,
-    textAlign:"center",
-    marginBottom:12
-  }
+  footerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
